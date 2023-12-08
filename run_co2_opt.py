9950dd810a0b7c8007d3d66cc4b2da56409ef23f
Jade Chongsathapornpong <jade.chongsathapornpong@gmail.com>
2023-12-08 01:23:10 -0500

    Fine line search + analysis

---
+++ run_co2_opt.py
@@ -0,0 +1,88 @@
+# Restricted Hartree-Fock
+from SCF import SCF
+
+import numpy as np
+from psi4.core import Molecule, BasisSet
+
+import csv
+import os.path
+
+CONVERGENCE = 2e-7 # Hartrees
+CONVERGENCE_2 = 1e-9
+
+co2elez = [8, 6, 8]
+
+if not os.path.isfile('co2line_coarse.csv'):
+    # define CO2 line search
+    co2_bondlengths = [1.4 - i * 0.003 for i in range(100)]
+    co2xyz = [[[-length, 0, 0], [0, 0, 0], [length, 0, 0]] for length in co2_bondlengths]
+    
+    co2_opt_energies = []
+
+    for idx, xyz in enumerate(co2xyz):
+        print("Computing energy for CO2 bond length co2_bondlength:", co2_bondlengths[idx])
+        co2 = Molecule.from_arrays(geom=xyz, elez=co2elez)
+        N_elec = np.sum(co2elez)
+        basisset = BasisSet.build(co2, target='sto-3g', quiet=idx > 0)
+
+        # set up the calculation
+        co2_HF = SCF(co2, N_elec, basisset)
+        co2_HF.run_SCF_iteration()
+        co2_HF.run_SCF_iteration()
+
+        while abs(co2_HF.energies[-1] - co2_HF.energies[-2]) > CONVERGENCE:
+            co2_HF.run_SCF_iteration()
+
+        co2_HF.check_consistency()
+        print(co2_HF.energies)
+        co2_opt_energies.append(co2_HF.energies[-1])
+
+    with open('co2line_coarse.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(("length", "energy"))
+        for lengthenergy in zip(co2_bondlengths, co2_opt_energies):
+            writer.writerow(lengthenergy)
+
+# perform fine CO2 line search
+if os.path.isfile('co2line_coarse.csv') and not os.path.isfile('co2line_fine.csv'):
+    # define new search based on the coarse output
+    co2_opt_energies = []
+    co2_bondlengths = []
+    with open('co2line_coarse.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        next(reader) # skip the header
+        for row in reader:
+            co2_bondlengths.append(float(row[0]))
+            co2_opt_energies.append(float(row[1]))
+    opt_idx = np.argmin(co2_opt_energies)
+    center = co2_bondlengths[opt_idx]
+    print("\nPERFORMING FINE LINE SEARCH\n")
+
+    fine_bondlengths = np.linspace(center - 0.006, center + 0.006, num=40)
+    fine_co2xyz = [[[-length, 0, 0], [0, 0, 0], [length, 0, 0]] for length in fine_bondlengths]
+    fine_co2_opt_energies = []
+
+    for xyz in fine_co2xyz:
+        print("Computing energy for CO2 bond length:", xyz[-1][0])
+        co2 = Molecule.from_arrays(geom=xyz, elez=co2elez)
+        N_elec = np.sum(co2elez)
+        basisset = BasisSet.build(co2, target='sto-3g', quiet=True)
+
+        # set up the calculation
+        co2_HF2 = SCF(co2, N_elec, basisset)
+        co2_HF2.run_SCF_iteration()
+        co2_HF2.run_SCF_iteration()
+
+        while abs(co2_HF2.energies[-1] - co2_HF2.energies[-2]) > CONVERGENCE_2:
+            co2_HF2.run_SCF_iteration()
+
+        fine_co2_opt_energies.append(co2_HF2.energies[-1])
+
+    with open('co2line_fine.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(("length", "energy"))
+        for lengthenergy in zip(fine_bondlengths, fine_co2_opt_energies):
+            writer.writerow(lengthenergy)
+
+
+
