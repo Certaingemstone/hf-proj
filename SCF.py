80e181bbfc5811c16664c602d4f3fb1aae4e352a
Jade Chongsathapornpong <jade.chongsathapornpong@gmail.com>
2023-12-08 00:34:42 -0500

    CO2 Line Search

---
+++ SCF.py
@@ -0,0 +1,83 @@
+import numpy as np
+import psi4
+from psi4.core import MintsHelper, Matrix, Vector
+
+
+class SCF:
+    # For performing and retrieving information about HF SCF calculation
+    def __init__(self, molecule: psi4.core.Molecule, N_elec: int, basis: psi4.core.BasisSet):
+        # read molecule and basis details
+        assert molecule.multiplicity() == 1
+        self.mol = molecule
+        assert N_elec % 2 == 0
+        self.N_occ = N_elec // 2
+        self.basis = basis
+        # calculate integrals
+        mints = MintsHelper(self.basis)
+        self.ENuclear = self.mol.nuclear_repulsion_energy()
+        h = mints.ao_kinetic()
+        h.add(mints.ao_potential())
+        self.h = h
+        self.S = mints.ao_overlap() # n x n
+        self.ERI = mints.ao_eri().to_array() # n x n x n x n two-electron integrals 
+        self.nbf = self.S.shape[0]
+
+        # prepare for iterations
+        # use the core-Hamiltonian guess, where the initial C*C density matrix vanishes
+        self.Fock = self.h.clone()
+        self.Fock_eigvals = Vector.from_array(np.zeros(self.nbf))
+        self.Fock_eigvects = Matrix.from_array(np.zeros((self.nbf, self.nbf)))
+
+        # Orthogonalize the basis implicitly via canonical orthogonalization
+        # diagonalize overlap
+        _ = Vector(self.nbf)
+        U = Matrix(self.nbf, self.nbf)
+        self.S.diagonalize(U, _, order = psi4.core.DiagonalizeOrder.Ascending)
+        Lambda = psi4.core.triplet(U, self.S, U, transA=True)
+        if np.any(np.less(np.diag(Lambda.to_array()), 1e-6)):
+            print("WARNING: Small overlap eigenvalue. This code does not truncate orthogonalizing matrix. Be wary of numerical issues.")
+        # use the diagonal overlap matrix to compute S^-1/2 in diagonal basis
+        Lambda_invsqrt = Matrix.from_array(np.diag(np.power(np.diag(Lambda.to_array()), -0.5)))
+        # compute symmetric orthogonalizing matrix 
+        self.X = psi4.core.triplet(U, Lambda_invsqrt, U, transC=True)
+
+        self.C = None
+        self.energies = []        
+
+
+    def run_SCF_iteration(self):
+
+        FockPrime = psi4.core.triplet(self.X, self.Fock, self.X, transA=True)
+        FockPrime.diagonalize(self.Fock_eigvects, self.Fock_eigvals, order = psi4.core.DiagonalizeOrder.Ascending)
+        # return these molecular orbitals to our original basis
+        self.C = psi4.core.doublet(self.X, self.Fock_eigvects)
+
+        ### Update the Fock matrix using the expansion coefficients in our basis set ###
+        # Crop to only the occupied MOs
+        C_np = self.C.to_array()
+        C_occupied = C_np[:, 0:self.N_occ]
+        # Compute density matrix
+        Density = Matrix.from_array(np.matmul(C_occupied, C_occupied.T))
+        # Compute the update to the non-core portion of the Hamiltonian
+        # there's probably a way to use matrix multiplications?
+        for mu in range(self.nbf):
+            for nu in range(self.nbf):
+                update = self.h.get(mu, nu)
+                for rho in range(self.nbf):
+                    for sigm in range(self.nbf):
+                        update += Density.get(rho, sigm) * (2 * self.ERI[mu][nu][rho][sigm] - self.ERI[mu][rho][nu][sigm])
+                self.Fock.set(mu, nu, update)
+
+        ### Calculate electronic energy ###
+        newE = Density.vector_dot(self.h) + Density.vector_dot(self.Fock) + self.ENuclear
+        self.energies.append(newE)
+
+
+    def check_consistency(self):
+        SCe = psi4.core.triplet(self.S, self.C, Matrix.from_array(np.diag(self.Fock_eigvals.to_array())))
+        FC = psi4.core.doublet(self.Fock, self.C)
+        one = SCe.to_array()
+        two = FC.to_array()
+        print("SCe:", np.sum(abs(one)))
+        print("FC:", np.sum(abs(two)))
+        print("Difference:", np.sum(abs(two-one)))
--- main.py
+++ main.py
@@ -1,105 +1,39 @@
 # Restricted Hartree-Fock
+from SCF import SCF
 
 import numpy as np
-import psi4
-from psi4.core import Molecule, BasisSet, Wavefunction, MintsHelper, Matrix, Vector
+from psi4.core import Molecule, BasisSet
 
-### Initialization ###
+import csv
 
-# define CO2
-co2xyz = [[-1.1, 0, 0], [0, 0, 0], [1.1, 0, 0]]
+CONVERGENCE = 2e-7 # Hartrees
+
+# define CO2 line search
+co2_bondlengths = [1.4 - i * 0.003 for i in range(100)]
+co2xyz = [[[-length, 0, 0], [0, 0, 0], [length, 0, 0]] for length in co2_bondlengths]
 co2elez = [8, 6, 8]
-co2 = Molecule.from_arrays(geom=co2xyz, elez=co2elez)
-N_elec = np.sum(co2elez)
-mol = co2
+co2_opt_energies = []
 
-assert N_elec % 2 == 0
-assert mol.multiplicity() == 1
-N_occ = N_elec // 2
+for idx, xyz in enumerate(co2xyz):
+    print("Computing energy for CO2 bond length co2_bondlength:", co2_bondlengths[idx])
+    co2 = Molecule.from_arrays(geom=xyz, elez=co2elez)
+    N_elec = np.sum(co2elez)
+    basisset = BasisSet.build(co2, target='sto-3g', quiet=idx > 0)
 
-energies = []
+    # set up the calculation
+    co2_HF = SCF(co2, N_elec, basisset)
+    co2_HF.run_SCF_iteration()
+    co2_HF.run_SCF_iteration()
 
-# Library calls to form the corresponding STO-3G basis
-basisset = BasisSet.build(mol, target='sto-3g')
+    while abs(co2_HF.energies[-1] - co2_HF.energies[-2]) > CONVERGENCE:
+        co2_HF.run_SCF_iteration()
 
-# and compute the nuclear repulsion energy from this geometry
-ENuclear = mol.nuclear_repulsion_energy()
+    co2_HF.check_consistency()
+    print(co2_HF.energies)
+    co2_opt_energies.append(co2_HF.energies[-1])
 
-# and to compute on our basis set for the molecule...
-mints = MintsHelper(basisset)
-# one-electron Hamiltonian matrix elements
-h = mints.ao_kinetic()
-h.add(mints.ao_potential())
-# overlap between spatial basis fns
-S = mints.ao_overlap()
-# electron repulsion integrals
-ERI = mints.ao_eri().to_array() # nbf x nbf x nbf x nbf
-
-wfn = Wavefunction.build(mol, basisset)
-
-# use the core-Hamiltonian guess, where the initial C*C density matrix vanishes
-Fock = h.clone()
-
-
-### Orthogonalize the basis implicitly via canonical orthogonalization ###
-
-dimS = S.shape[0]
-# diagonalize overlap
-S_eigvals = Vector(dimS)
-U = Matrix(dimS,dimS)
-S.diagonalize(U, S_eigvals, order = psi4.core.DiagonalizeOrder.Ascending)
-Lambda = psi4.core.triplet(U, S, U, transA=True)
-
-if np.any(np.less(np.diag(Lambda.to_array()), 1e-6)):
-    print("WARNING: Small overlap eigenvalue. This code does not truncate orthogonalizing matrix. Be wary of numerical issues.")
-
-# use the diagonal overlap matrix to compute S^-1/2 in diagonal basis
-Lambda_invsqrt = Matrix.from_array(np.diag(np.power(np.diag(Lambda.to_array()), -0.5)))
-# compute symmetric orthogonalizing matrix 
-X = psi4.core.triplet(U, Lambda_invsqrt, U, transC=True)
-
-### Diagonalize the Fock matrix in the orthogonal basis to obtain molecular orbitals, then return to our basis set ###
-
-newE = 0
-niter = 0
-
-dimF = Fock.shape[0]
-Fock_eigvals = Vector.from_array(np.zeros(dimF))
-Fock_eigvects_Cprime = Matrix.from_array(np.zeros((dimF, dimF)))
-
-while niter < 10:
-    FockPrime = psi4.core.triplet(X, Fock, X, transA=True)
-    FockPrime.diagonalize(Fock_eigvects_Cprime, Fock_eigvals, order = psi4.core.DiagonalizeOrder.Ascending)
-    # return these molecular orbitals to our original basis
-    C = psi4.core.doublet(X, Fock_eigvects_Cprime)
-
-    ### Update the Fock matrix using the expansion coefficients in our basis set ###
-    # Crop to only the occupied MOs
-    C_np = C.to_array()
-    C_occupied = C_np[:, 0:N_occ]
-    # Compute density matrix
-    Density = Matrix.from_array(np.matmul(C_occupied, C_occupied.T))
-    # Compute the update to the non-core portion of the Hamiltonian
-    # there's probably a way to use matrix multiplications?
-    for mu in range(dimF):
-        for nu in range(dimF):
-            update = h.get(mu, nu)
-            for rho in range(dimF):
-                for sigm in range(dimF):
-                    update += Density.get(rho, sigm) * (2 * ERI[mu][nu][rho][sigm] - ERI[mu][rho][nu][sigm])
-            Fock.set(mu, nu, update)
-
-    ### Calculate electronic energy ###
-    newE = Density.vector_dot(h) + Density.vector_dot(Fock) + ENuclear
-    energies.append(newE)
-    niter += 1
-
-print(energies)
-# perform a check that the FC ~= SCe
-SCe = psi4.core.triplet(S, C, Matrix.from_array(np.diag(Fock_eigvals.to_array())))
-
-FC = psi4.core.doublet(Fock, C)
-
-one = SCe.to_array()
-two = FC.to_array()
-print(two - 1)
+with open('co2line.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(("length", "energy"))
+    for lengthenergy in zip(co2_bondlengths, co2_opt_energies):
+        writer.writerow(lengthenergy)
